59858a7ff2bd0d187bba47126fc74385f2561973
aidenchia <aidenchia95@gmail.com>
2020-04-13 04:12:30 +0800

    connected to blockchain and able to call contract methods

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
+import SocialNetwork from '../abis/SocialNetwork.json';
 import Navbar from './Navbar';
 
 class App extends Component {
@@ -36,11 +37,44 @@ class App extends Component {
 
     // set to state
     this.setState({account: accounts[0]})
+
+    // Network ID 
+    const networkId = await web3.eth.net.getId()
+    const networkData  = SocialNetwork.networks[networkId]
+
+    // Address
+    if (networkData) {
+      const socialNetwork = web3.eth.Contract(SocialNetwork.abi, networkData.address)
+      this.setState({socialNetwork: socialNetwork})
+      
+      // need to know number of posts to list them
+      const numPosts = await socialNetwork.methods.numPosts().call() // call methods read blockchain
+      this.setState({numPosts})
+
+      // set state of all posts
+      for (var i = 1; i <= numPosts; i++) {
+        const post = await socialNetwork.methods.posts(i).call()
+        this.setState({
+          posts: [...this.state.posts, post] // ES6 allows us to add an element to array with spread operator
+        })
+      }
+
+
+    } else {
+      window.alert('Social Network contract has not been deployed to detected network')
+    }
+
+    // ABI
   }
 
   constructor(props) {
     super(props) 
-    this.state = {account: ''}
+    this.state = {
+      account: '',
+      socialNetwork: null,
+      numPosts: 0,
+      posts: []
+    }
   }
 
 
